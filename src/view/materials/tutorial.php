91f88ea66b508b1a5dec79facc97f6851e624926
Thibaut Degryse <thibaut.degryse@student.howest.be>
2021-01-25 17:51:56 +0100

    added HTML and CSS to the tutorial page

--- src/view/materials/tutorial.php
+++ src/view/materials/tutorial.php
@@ -1,188 +1,161 @@
+
+
 <div class="container">
-<article>
-<h1 class="hidden">De drijvende strandstoel tutorial</h1>
-    <section>
-        <button class="button__previous" href="">vorige stap</button>
-        <button class="button__next">volgende stap</button>
-       <a class="hidden button__end" href="index.php" class="hidden button__end">terug naar de homepage</a>
-    </section>
-
-<section class="step1">
-
-<h2>Stap 1: Het drijfsysteem</h2>
-
-<p>
-Om het hele boeltje drijvende te houden moeten we onderaan de drijfbanden aanhangen. Boor 4 gaten van 10mm en haal de 2 langste staven erdoor.
-</p>
-
-<img src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
-
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/staven.png" alt="drijfband">
-                     <p class="materiaal__naam">drijfband</p>
-                  </div>
-</section>
-
-
-
-<section class="step2 hidden">
-
-<h2>Stap 2: Het drijfsysteem</h2>
-
-<p>
-Nadat je de staven hebt bevestigd
-blaas je de 4 drijfbanden op. En 
-bevestig je ze aan de staven met
-plakband. (gebruik er genoeg)</p>
-
-<img src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
-
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/drijfband.png" alt="drijfband">
-                     <p class="materiaal__naam">drijfband</p>
-                  </div>
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/plakband.png" alt="plakband">
-                     <p class="materiaal__naam">plakband</p>
-                  </div>
-
-</section>
-
-
-
-
-
-
-
-
-
-
-
-<section class="step3 hidden">
-<h2>Stap 3: Het drijfsysteem</h2>
-
-<p>
-Nadat je de staven hebt bevestigd
-blaas je de 4 drijfbanden op. En 
-bevestig je ze aan de staven met
-plakband. (gebruik er genoeg)</p>
-
-<img src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
-
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/drijfband.png" alt="drijfband">
-                     <p class="materiaal__naam">drijfband</p>
-                  </div>
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/plakband.png" alt="plakband">
-                     <p class="materiaal__naam">plakband</p>
-                  </div>
-                  </section>
-
-
-
-
-
-
-<section class="step4 hidden">
-
-<h2>Stap 4: Het drijfsysteem</h2>
-
-<p>
-Nadat je de staven hebt bevestigd
-blaas je de 4 drijfbanden op. En 
-bevestig je ze aan de staven met
-plakband. (gebruik er genoeg)</p>
-
-<img src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
-
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/drijfband.png" alt="drijfband">
-                     <p class="materiaal__naam">drijfband</p>
-                  </div>
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/plakband.png" alt="plakband">
-                     <p class="materiaal__naam">plakband</p>
-                  </div>
-                  </section>
-
-
-
-<section class="step5 hidden">
-<h2>Stap 5: Het drijfsysteem</h2>
-
-<p>
-Nadat je de staven hebt bevestigd
-blaas je de 4 drijfbanden op. En 
-bevestig je ze aan de staven met
-plakband. (gebruik er genoeg)</p>
-
-<img src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
-
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/drijfband.png" alt="drijfband">
-                     <p class="materiaal__naam">drijfband</p>
-                  </div>
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/plakband.png" alt="plakband">
-                     <p class="materiaal__naam">plakband</p>
-                  </div>
-</section>
-
-
-
-<section class="step6 hidden">
-<h2>Stap 6: Het drijfsysteem</h2>
-
-<p>
-Nadat je de staven hebt bevestigd
-blaas je de 4 drijfbanden op. En 
-bevestig je ze aan de staven met
-plakband. (gebruik er genoeg)</p>
-
-<img src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
-
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/drijfband.png" alt="drijfband">
-                     <p class="materiaal__naam">drijfband</p>
-                  </div>
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/plakband.png" alt="plakband">
-                     <p class="materiaal__naam">plakband</p>
-                  </div>
-</section>
-
-
-
-
-<section class="step7 hidden">
-<h2>Stap 7: Het drijfsysteem</h2>
-
-<p>
-Nadat je de staven hebt bevestigd
-blaas je de 4 drijfbanden op. En 
-bevestig je ze aan de staven met
-plakband. (gebruik er genoeg)</p>
-
-<img src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
-
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/drijfband.png" alt="drijfband">
-                     <p class="materiaal__naam">drijfband</p>
-                  </div>
-                  <div class="materiaal__wrapper">
-                     <img width="166" height="166" src="assets/images/plakband.png" alt="plakband">
-                     <p class="materiaal__naam">plakband</p>
-                  </div>
-
-</section>
-
-
-
-
-
-
-
-</article>
-
+   <div class="container--tutorial">
+      <article>
+         <h1 class="hidden">De drijvende strandstoel tutorial</h1>
+         <div class="button__wrapper">
+            <button class="button button--meer button__previous" href="">vorige stap</button>
+            <button class="button button--tut button__next">volgende stap</button>
+            <a class="hidden button__end" href="index.php" class="hidden button__end">terug naar de homepage</a>
+         </div>
+         <section class="step step1">
+            <div class="tut__wrapper">
+               <h2 class="titel__tutorial">Stap 1: Het drijfsysteem</h2>
+               <p class="tutorial__uitleg">
+                  Om het hele boeltje drijvende te houden moeten we onderaan de drijfbanden aanhangen. Boor 4 gaten van 10mm en haal de 2 langste staven erdoor.
+               </p>
+            </div>
+            <img class="tutorial__grid" src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
+            <div class="materiaal--lijst__wrapper">
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/drijfband.png" alt="drijfband">
+                  <p class="materiaal__naam">drijfband</p>
+               </div>
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/staven.png" alt="drijfband">
+                  <p class="materiaal__naam">drijfband</p>
+               </div>
+            </div>
+         </section>
+         <section class="step step2 hidden">
+            <div class="tut__wrapper">
+               <h2 class="titel__tutorial">Stap 2: Het drijfsysteem</h2>
+               <p class="tutorial__uitleg">
+                  Nadat je de staven hebt bevestigd
+                  blaas je de 4 drijfbanden op. En 
+                  bevestig je ze aan de staven met
+                  plakband. (gebruik er genoeg)         
+               </p>
+            </div>
+            <img class="tutorial__grid" src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
+            <div class="materiaal--lijst__wrapper">
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/drijfband.png" alt="drijfband">
+                  <p class="materiaal__naam">drijfband</p>
+               </div>
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/plakband.png" alt="plakband">
+                  <p class="materiaal__naam">plakband</p>
+               </div>
+            </div>
+         </section>
+         <section class="step step3 hidden">
+            <div class="tut__wrapper">
+               <h2 class="titel__tutorial">Stap 3: Het stuursysteem</h2>
+               <p class="tutorial__uitleg">
+                  Boor een 10mm gat in de leuning van
+                  de stoel. Hier haal je de hendel door.
+                  Schaaf genoeg oppervlak af dat de hendel
+                  360 graden vrij kan draaien.  
+               </p>
+            </div>
+            <img class="tutorial__grid" src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
+            <div class="materiaal--lijst__wrapper">
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/hendel.png" alt="hendel">
+                  <p class="materiaal__naam">hendel</p>
+               </div>
+            </div>
+         </section>
+         <section class="step step4 hidden">
+            <div class="tut__wrapper">
+               <h2 class="titel__tutorial">Stap 4: Het stuursysteem</h2>
+               <p class="tutorial__uitleg">
+                  De hendel moet nu vastgehangen worden
+                  aan een stuurbout. Deze stuurbout schroef
+                  je vast aan de onderkant van de hendel. Hier
+                  haal je ook meteen de stuurstaaf door
+               </p>
+            </div>
+            <img class="tutorial__grid" src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
+            <div class="materiaal--lijst__wrapper">
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/staven.png" alt="staven">
+                  <p class="materiaal__naam">staven</p>
+               </div>
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/schroeven.png" alt="schroeven">
+                  <p class="materiaal__naam">schroeven</p>
+               </div>
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/stuurbout.png" alt="stuurbout">
+                  <p class="materiaal__naam">stuurbout</p>
+               </div>
+            </div>
+         </section>
+         <section class="step step5 hidden">
+            <div class="tut__wrapper">
+               <h2 class="titel__tutorial">Stap 5: De stuurvin</h2>
+               <p class="tutorial__uitleg">
+                  Hang aan het uiteinde van 
+                  de laatst gemonteerde staaf
+                  de stuursleutel. Haal deze tussen
+                  de poot en het stof van de stoel en
+                  sluit de sleutel er rond.
+               </p>
+            </div>
+            <img class="tutorial__grid" src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
+            <div class="materiaal--lijst__wrapper">
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/sleutel.png" alt="sleutel">
+                  <p class="materiaal__naam">sleutel</p>
+               </div>
+            </div>
+         </section>
+         <section class="step step6 hidden">
+            <div class="tut__wrapper">
+               <h2 class="titel__tutorial">Stap 6: De stuurvin</h2>
+               <p class="tutorial__uitleg">
+                  Gebruik de laatste staaf als houder.
+                  Haal het door het uiteinde van de sleutel en
+                  schroef vast. Op het andere uiteinde van
+                  de staaf steek je de andere stuurbout. Aan
+                  die stuurbout hang je je stuurvin.
+               </p>
+            </div>
+            <img class="tutorial__grid" src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
+            <div class="materiaal--lijst__wrapper">
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/stuurbout.png" alt="stuurbout">
+                  <p class="materiaal__naam">stuurbout</p>
+               </div>
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/staven.png" alt="staven">
+                  <p class="materiaal__naam">staven</p>
+               </div>
+               <div class="materiaal__wrapper">
+                  <img width="166" height="166" src="assets/images/schroeven.png" alt="schroeven">
+                  <p class="materiaal__naam">schroeven</p>
+               </div>
+            </div>
+         </section>
+         <section class="step step7 hidden">
+            <div class="tut__wrapper">
+               <h2 class="titel__tutorial">Stap 7: Accessoires</h2>
+               <p class="tutorial__uitleg">
+                  Als accessoires kan je veel doen.
+                  Wij hebben onze stoel gepimpt met 
+                  een parasol, bekerhouder en zeilen.
+                  Jij kan hier misschien nog veel verder
+                  in gaan. Post jouw foto op Instagram met
+                  #MakerChair en maak kans op een maker
+                  experience met Henk en Lieven!
+               </p>
+            </div>
+            <img class="tutorial__grid" src="assets/grids/tutorial-grid.svg" alt="een grid die de afbeeldingen ophoud">
+         </section>
+      </article>
+   </div>
 </div>
+
